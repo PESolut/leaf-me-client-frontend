be3ae8803b4ef874a83b29216b12a97b90341183
PESolut <jahaadpetty@pursuit.org>
2023-07-25 18:48:05 -0400

    basketIndex - created getCurrentBasketNames function - returns an array of basketNames. basketItems[i] elements share the same index as basketNameArray

--- src/Components/Basket/BasketIndex.js
+++ src/Components/Basket/BasketIndex.js
@@ -5,7 +5,7 @@ import BasketItem from './BasketItem.js';
 import './BasketIndex.css'
 
 const BasketIndex = () => {
-    const {userID, axios, API, basket, setStoreItems, storeItems} = useContextProvider()
+    const {userID, axios, API, basket, storeItems} = useContextProvider()
     const [currentBasket, setCurrentBasket] = useState ({
         total: null,
         itemCount: null,
@@ -18,12 +18,10 @@ const BasketIndex = () => {
             axios
             .get(`${API}/users/${userID}/basket/${basket[0].id}/storeitems`)
             .then(({data}) => {
-
                 setCurrentBasket({
                     total: currentBasket.total,
                     itemCount: currentBasket.itemCount,
                     items: data
-                    
                 })
             })
             .catch((error) => {
@@ -33,6 +31,7 @@ const BasketIndex = () => {
     },[userID, basket])
 
     // on currentBasketItems change, append total to basket object
+    // on currentBasketItems change, fetch basket items
     useEffect(()=> {
         if(currentBasket.items.length > 0){
             const total = (calculateTotalBasketItemsPrice(currentBasket.items))
@@ -41,6 +40,9 @@ const BasketIndex = () => {
                 itemCount: 0,
                 items: currentBasket.items
             })
+        }
+        if(currentBasket.items.length > 0){
+            const basketNamesArr = getCurrentBasketNames(currentBasket.items)
         }
     },[userID, basket, currentBasket.items])
 
@@ -56,16 +58,37 @@ const BasketIndex = () => {
         .catch((error) => {
             console.error(error)
         })
-        
+        // iterate over basketItems, on each element of basketItem, take values quantity and store_item_id
+        // use those to find our store_item.price value and multipy that value by the quantity value
+        // return total integer in dollar format
         for (let i = 0; i < basketItems.length; i++) {
           const { quantity, store_item_id } = basketItems[i];
           const currentElPrice = storeItems[store_item_id-1].price * quantity
         total += currentElPrice 
         }
-        // console.log(total)
         return total.toFixed(2);
         }
       };
+
+    const getCurrentBasketNames = (basketItems) => {
+        let returnArr = []
+        axios
+        .get(`${API}/allstoreitems`)
+        .then(({data}) => {
+            const storeItems = data
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+        // iterate over basketItems, on each element of basketItem, take value store_item_id
+        for(let i = 0; i < basketItems.length; i++) {
+            const { store_item_id } = basketItems[i]
+            const currentElName = storeItems[store_item_id-1].name
+            returnArr.push(currentElName)
+        }
+        return returnArr
+    }
+
     return (
         <>
         <section className='basket-total-container'>
